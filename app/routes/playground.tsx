b6fb08a2eb291e5ea5b6743624887da1c68f8a08
PerryM123 <perrymiranda1@yahoo.com>
2026-03-20 15:26:50 +0900

    we have cubes now

--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -1,4 +1,9 @@
 import type { Route } from './+types/home'
+import { Canvas, useFrame } from '@react-three/fiber'
+// TODO: Setup aliases
+import './../../app/three-fiber-style.css'
+import { useRef, useState } from 'react'
+import type { Mesh } from 'three'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +12,51 @@ export function meta({}: Route.MetaArgs) {
   ]
 }
 
+const Box = ({ position }: { position: [x: number, y: number, z: number] }) => {
+  console.log('perry: Box component')
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef<Mesh | null>(null)
+  console.log('perry: meshRef: ', meshRef)
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    if (!meshRef.current) {
+      return
+    }
+    meshRef.current.rotation.x += delta
+  })
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      position={position}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
 export default function Home() {
-  return <div>hello world</div>
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
 }
